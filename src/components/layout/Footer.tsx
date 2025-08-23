bc5fe580e995f3faf70f1efab30c02c9e9d33225
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-08-23 21:07:31 +0300

    fix: change Header/Footer logo to custom logo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -21,9 +21,10 @@ export const Footer: React.FC = () => {
                             <div className="w-12 h-12 rounded-3xl flex items-center justify-center shadow-lg" style={{
                                 background: 'linear-gradient(135deg, #8B5CF6 0%, #3B82F6 100%)'
                             }}>
-                                <svg className="w-7 h-7 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
-                                </svg>
+                                {/*<svg className="w-7 h-7 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">*/}
+                                {/*    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />*/}
+                                {/*</svg>*/}
+                                <img src="/WorkApp2.png"  className=" h-22 rounded-xl shadow-lg"/>
                             </div>
                             <div>
                                 <h3 className="text-lg font-bold text-gray-800">WorkApp</h3>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -75,7 +75,7 @@ export const Header: React.FC<HeaderProps> = ({ onLogout }) => {
                             {/*<svg className="w-7 h-7 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">*/}
                             {/*    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />*/}
                             {/*</svg>*/}
-                            <img src="/WorkApp2.png" alt="Logo" />
+                            <img src="/WorkApp2.png"  className=" h-22 rounded-xl shadow-lg"/>
                         </div>
                         <div className="hidden sm:block">
                             <h1 className="text-xl font-bold text-white">WorkApp</h1>
